ce37d2231b63aa06e96e2100970ab9e0196d3800
3epr <tpynope31@gmail.com>
2021-03-02 13:23:10 +0300

    new model html

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
-from .models import Bb
+from .models import Bb, Rublic
 
 def index(request):
  bbs = Bb.objects.all()
  return render(request, 'bboard/index.html', {'bbs': bbs})
+
+def by_rublic(request, rublic_id):
+ bbs = Bb.objects.filter(rublic=rublic_id)
+ rublics = Rublic.objects.all()
+ current_rublic = Rublic.objects.get(pk=rublic_id)
+ context = {'bbs': bbs, 'rublics': rublics, 'current_rublic': current_rublic}
+ return render(request, 'bboard/by_rublic.html', context)
